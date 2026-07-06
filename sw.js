a5c2646e81d2f7c1f3fd5fa64e280475b54f423d
akarklinss <akarklins22@gmail.com>
2026-07-06 18:07:44 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,12 @@
-const CACHE_NAME = "parking-gate-v1";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./api.js",
-  "./camera.js",
-  "./manifest.json"
-];
-
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }))
+      Promise.all(keys.map(key => caches.delete(key)))
     )
   );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  self.clients.claim();
 });
