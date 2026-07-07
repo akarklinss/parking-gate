3dd6777d97046910b2b5a5defe1fa8d4232cdf1c
akarklinss <akarklins22@gmail.com>
2026-07-07 16:08:16 +0300

    api.js

--- api.js
+++ api.js
@@ -2,34 +2,42 @@ const API_URL = "https://script.google.com/macros/s/AKfycbyX9nF5tnfuU0X9T9Fg1z4g
 
 function jsonpRequest(params) {
   return new Promise((resolve, reject) => {
-    const callbackName = "pgCallback_" + Date.now();
-
-    params.callback = callbackName;
-    params.cachebuster = Date.now();
-
-    const query = new URLSearchParams(params).toString();
-    const script = document.createElement("script");
+    const callbackName = "pgCallbackTest" + Date.now();
 
     window[callbackName] = function(data) {
+      resolve(data);
       delete window[callbackName];
       script.remove();
-      resolve(data);
+    };
+
+    const script = document.createElement("script");
+
+    const query =
+      Object.keys(params)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(params[key]))
+        .join("&") +
+      "&callback=" + callbackName +
+      "&_=" + Date.now();
+
+    script.src = API_URL + "?" + query;
+
+    script.onload = function() {
+      // JSONP callback jau apstrādā atbildi
     };
 
     script.onerror = function() {
+      reject(new Error("API skripts neielādējās. URL vai piekļuve nav pareiza."));
       delete window[callbackName];
       script.remove();
-      reject(new Error("API skripts neielādējās. Pārbaudi API_URL vai deployment piekļuvi."));
     };
 
-    script.src = API_URL + "?" + query;
-    document.body.appendChild(script);
+    document.head.appendChild(script);
 
     setTimeout(() => {
       if (window[callbackName]) {
+        reject(new Error("API neatbildēja 15 sekunžu laikā."));
         delete window[callbackName];
         script.remove();
-        reject(new Error("API neatbildēja 15 sekunžu laikā."));
       }
     }, 15000);
   });
